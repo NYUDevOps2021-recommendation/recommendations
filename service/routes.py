4754197fbd833d9153f1affde973cfd4c9008860
legendary2017 <827788294@qq.com>
2021-10-16 00:25:41 -0400

    Created CREATE API

--- service/routes.py
+++ service/routes.py
@@ -18,6 +18,7 @@ from service.models import Recommendations, DataValidationError
 # Import Flask application
 from . import app
 
+
 ######################################################################
 # GET INDEX
 ######################################################################
@@ -31,11 +32,62 @@ def index():
 
 
 ######################################################################
+# RETRIEVE A RECOMMENDATION
+######################################################################
+@app.route("/recommendations/<int:id>", methods=["GET"])
+def get_recommendations(id):
+    """
+    Retrieve a single Recommendation
+    This endpoint will return a Recommendation based on it's id
+    """
+    app.logger.info("Request for recommendation with id: %s", id)
+    recommendation = Recommendations.find_by_id(id)
+    if not recommendation:
+        raise NotFound("Recommendation with id '{}' was not found.".format(id))
+    return make_response(jsonify(recommendation.serialize()), status.HTTP_200_OK)
+
+######################################################################
+# ADD A NEW RECOMMENDATION
+######################################################################
+@app.route("/recommendations", methods=["POST"])
+def create_recommendations():
+    """
+    Creates a recommendation
+    This endpoint will create a recommendation based the data in the body that is posted
+    """
+    app.logger.info("Request to create a recommendation")
+    check_content_type("application/json")
+    recommendation = Recommendations()
+    recommendation.deserialize(request.get_json())
+    recommendationList = Recommendations.find_by_attributes(recommendation.product_origin,
+                                                            recommendation.product_target,
+                                                            recommendation.relation)
+    if len(recommendationList) == 0:
+        recommendation.create()
+    else:
+        recommendation = recommendationList[0]
+        if recommendation.is_deleted == 1:
+            recommendation.is_deleted = 0
+            recommendation.save()
+    message = recommendation.serialize()
+    location_url = url_for("get_recommendations", id=recommendation.id, _external=True)
+    return make_response(
+        jsonify(message), status.HTTP_201_CREATED, {'Location': location_url}
+    )
+
+######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
 
 def init_db():
     """ Initialies the SQLAlchemy app """
     global app
     Recommendations.init_db(app)
+
+
+def check_content_type(content_type):
+    """ Checks that the media type is correct """
+    if "Content-Type" in request.headers and request.headers["Content-Type"] == content_type:
+        return
+    app.logger.error("Invalid Content-Type: [%s]", request.headers.get("Content-Type"))
+    abort(status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, "Content-Type must be {}".format(content_type))
