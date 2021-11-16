4246b35350aa0cb0e4262d49aceeb1ee6087b1a5
legendary2017 <827788294@qq.com>
2021-11-15 23:01:01 -0500

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ DATABASE_URI = os.getenv(
 )
 
 if 'VCAP_SERVICES' in os.environ:
- vcap = json.loads(os.environ['VCAP_SERVICES'])
- DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
+    vcap = json.loads(os.environ['VCAP_SERVICES'])
+    DATABASE_URI = vcap['user-provided'][0]['credentials']['url']
 
 # Configure SQLAlchemy
 SQLALCHEMY_DATABASE_URI = DATABASE_URI
